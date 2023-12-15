36487a1e3a9526c82de40b8777857d307b6e7199
Guterresthomas <guterresmikael@gmail.com>
2023-12-15 10:10:51 -0300

    att imgs

--- src/components/glassesCaroussel.tsx
+++ src/components/glassesCaroussel.tsx
@@ -6,15 +6,35 @@ export default function CarouselDefaultGlasses() {
   return (
     <Carousel className="rounded-xl">
       <img
-        src="/oculos1.jpg" alt="image 1"
+        src="/oculos5.jpg" alt="image 1"
         className="h-full w-full object-cover"
       />
       <img
-        src="/oculos2.jpg" alt="image 2"
+        src="/oculos6.jpg" alt="image 2"
         className="h-full w-full object-cover"
       />
       <img
-        src="/oculos4.jpg" alt="image 3"
+        src="/oculos7.jpg" alt="image 3"
+        className="h-full w-full object-cover"
+      />
+      <img
+        src="/oculos8.jpg" alt="image 4"
+        className="h-full w-full object-cover"
+      />
+      <img
+        src="/oculos9.jpg" alt="image 5"
+        className="h-full w-full object-cover"
+      />
+      <img
+        src="/oculos10.jpg" alt="image 6"
+        className="h-full w-full object-cover"
+      />
+      <img
+        src="/oculos11.jpg" alt="image 7"
+        className="h-full w-full object-cover"
+      />
+      <img
+        src="/oculos12.jpg" alt="image 8"
         className="h-full w-full object-cover"
       />
     </Carousel>
