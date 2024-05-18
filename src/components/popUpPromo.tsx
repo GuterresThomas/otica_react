a763719a5be83849c9be445de9f982067b0615ee
Guterresthomas <guterresmikael@gmail.com>
2024-05-17 23:30:49 -0300

    teste

--- src/components/popUpPromo.tsx
+++ src/components/popUpPromo.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import { useEffect, useRef } from 'react';
 import { Carousel } from "@material-tailwind/react";
 
@@ -27,7 +25,13 @@ export default function PopUpCard({ isOpen, onClose }: PopUpCardProps) {
 
     return (
         <div className="fixed inset-0 flex items-center justify-center bg-transparent bg-opacity-100 z-50">
-            <div ref={popupRef} className="bg-transparent p-4 rounded-xl">
+            <div ref={popupRef} className="bg-transparent p-4 rounded-xl relative">
+                <button
+                    onClick={onClose}
+                    className="absolute top-2 right-2 bg-white p-2 rounded-full text-gray-700 hover:bg-gray-200 focus:outline-none focus:bg-gray-200"
+                >
+                    Fechar
+                </button>
                 <Carousel>
                     <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '40rem' }}>
                         <img src="promo1.png" alt="Promo 1" style={{ maxHeight: '100%', maxWidth: '100%' }} />
