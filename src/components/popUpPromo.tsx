f5590c2298c7469cd3c62e02a7f57e86fa6c6ff5
Guterresthomas <guterresmikael@gmail.com>
2024-05-17 23:17:30 -0300

    testezin2

--- src/components/popUpPromo.tsx
+++ src/components/popUpPromo.tsx
@@ -1,14 +1,19 @@
-'use client'
+'use client';
 
 import { useEffect, useRef } from 'react';
 import { Carousel } from "@material-tailwind/react";
 
-export default function PopUpCard({ isOpen, onClose }) {
-    const popupRef = useRef();
+interface PopUpCardProps {
+    isOpen: boolean;
+    onClose: () => void;
+}
+
+export default function PopUpCard({ isOpen, onClose }: PopUpCardProps) {
+    const popupRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
-        function handleClickOutside(event) {
-            if (popupRef.current && !popupRef.current.contains(event.target)) {
+        function handleClickOutside(event: MouseEvent) {
+            if (popupRef.current && !popupRef.current.contains(event.target as Node)) {
                 onClose();
             }
         }
@@ -24,8 +29,8 @@ export default function PopUpCard({ isOpen, onClose }) {
         <div className="fixed inset-0 flex items-center justify-center bg-transparent bg-opacity-100 z-50">
             <div ref={popupRef} className="bg-transparent p-4 rounded-xl">
                 <Carousel>
-                     <img src="promo1.png" alt="Promo 1"  style={{ maxHeight: '40rem' }} className="object-cover" />
-                     <img src="promo2.png" alt="Promo 1"  style={{ maxHeight: '40rem' }} className="object-cover" />
+                    <img src="promo1.png" alt="Promo 1" style={{ maxHeight: '40rem' }} className="" />
+                    <img src="promo2.png" alt="Promo 2" style={{ maxHeight: '40rem' }} className="" />
                 </Carousel>
             </div>
         </div>
